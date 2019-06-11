10c7fc1e47c5549860310572fa286ebd415588f6
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-06-11 13:38:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,11 +128,9 @@ bot.on('guildMemberAdd', member => {
 	var regex = /([10-99])$/;
 	console.log(`regex:${regex}`);
 	function confirmEnding(string, target) {
-  	if (string.substr(-target.length) === target) {
-    	return true;
-	  } else {
-  	  return false;
-	  }
+  	// We return the method with the target as a parameter
+  	// The result will be a boolean (true/false)
+  	return string.endsWith(target); // 'Bastian'.endsWith('n')
 	}
 	if (confirmEnding(user, regex)){
 	 console.log(`found a bot`);
