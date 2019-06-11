1429093e128a09caa562bbb3cd22e3736b167b1a
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-06-11 12:20:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -134,7 +134,7 @@ bot.on('guildMemberAdd', member => {
  '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76',
  '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99',)){
 	 if (member.presence.status = 'offline'){
-		async function bean(){
+		async function bean(member){
 		 await message.guild.ban(member)}
 	 }
 	}
