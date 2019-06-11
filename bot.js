dd47cdc3c67e98304c7f00fa3fd6c50ffea082a2
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-06-11 12:26:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ bot.on('message', message => {
 bot.on('guildMemberAdd', member => {
 	user = member.user.username;
 	
-	console.log(`user: ${user} member: ${member}`)
+	console.log(`user: ${user} member: ${member}`);
 	if (user.endsWith('01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
  '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
  '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32',
